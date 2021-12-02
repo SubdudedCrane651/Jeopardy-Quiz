70841608fce76f80f4b0cf1b70e0f65db62f2625
SubduedCrane651 <95251533+SubdudedCrane651@users.noreply.github.com>
2021-12-02 23:05:24 +0000

    Added json function

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import json
 
 url = "https://richard-perreault.com/Documents/JEOPARDY_QUESTIONS1.json"
 
-request = requests.get(url)
-print(request.text)
+response = requests.get(url)
+data = json(response.text)
+print(data)
+#print(response.text)
