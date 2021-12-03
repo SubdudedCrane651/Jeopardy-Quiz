13ddcfaf10d6e0a88b90c4a6bce77b5a55508a69
SubduedCrane651 <95251533+SubdudedCrane651@users.noreply.github.com>
2021-12-03 00:56:55 +0000

    Almost done

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ count = len(data)
 rand = random.randrange(1,count)
 quiz = pd.DataFrame(data)
 
-print(quiz['category'][rand])
+print("Category: "+quiz['category'][rand]+"\n"
++"Air Date: "+quiz['air_date'][rand]+"\n"
++"Question: "+quiz['question'][rand]+"\n"
++"Value :"+quiz['value'][rand]+"\n"
++"Round :"+quiz['round'][rand]+"\n"
++"Show number :"+quiz['show_number'][rand]+"\n")
+
+input("<ENTER> for Answer")
+print("Answer :"+quiz['answer'][rand])
