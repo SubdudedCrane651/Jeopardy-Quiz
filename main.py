59e7f529e46d0774db54c4d75304cd1e2c326d5d
SubduedCrane651 <95251533+SubdudedCrane651@users.noreply.github.com>
2021-12-04 01:03:37 +0000

    Major bugs remove with function

--- main.py
+++ main.py
@@ -5,57 +5,93 @@ import random
 
 url = "https://richard-perreault.com/Documents/JEOPARDY_QUESTIONS1.json"
 
+global wager
+wager=""
+
 response = requests.get(url)
 data = json.loads(response.text)
 
+global count
 count = len(data)
 quiz = pd.DataFrame(data)
 
-print("[1] 100 [2] 200 [3] 400 [4] 500 [5] 600 [6] 700 [7] 800 [8] 1000 [9] 1200")
-wager =input("How much do you want to wager ")
+def quizchoice():
+  print("[1] 100 [2] 200 [3] 300 [4] 400 [5] 500 [6] 600 [7] 800 [8] 1000 [9] 1200 [10] 1600 [11] 2000")
+  wager =input("How much do you want to wager ")
 
-go=True 
+  go=True 
 
-while go:
- rand = random.randrange(1,count) 
- value = quiz['value'][rand]
- #removedollar = ''.join([value for i in range(len(value)) if i != 0])
- removedollar =  value.replace('$', '')
- removecomma =  removedollar.replace(',', '')
- try:
-  if int(wager) == 1:    
-    if int(removecomma) == 100:
-      go = False
-  elif int(wager) == 2:
-    if int(removecomma) == 200:
-      go = False
-  elif int(wager) == 3:
-    if int(removecomma) == 400:
-      go = False
-  elif int(wager) == 4:
-    if int(removecomma) == 500:
-      go = False
-  elif int(wager) == 5:
-    if int(removecomma) == 600:
-      go = False
-  elif int(wager) == 6:
-    if int(removecomma) == 700:
-      go = False      
-  elif int(wager) == 7:
-    if int(removecomma) == 800:
-      go = False
-  elif int(wager) == 8:
-    if int(removecomma) == 1000:
-      go = False 
-  elif int(wager) == 9:
-    if int(removecomma) == 1200:
-      go = False
-  elif int(wager) == 10:
-    if int(removecomma) == 1400:
-      go = False
- except:
-  go=Tue            :
+  while go:
+    global rand
+    rand = random.randrange(1,count+1) 
+    value = quiz['value'][rand]
+    #removedollar = ''.join([value for i in range(len(value)) if i != 0])
+    removedollar = value.replace('$', '')
+    try:
+     removecomma = removedollar.replace(',', '')
+    except:
+       removecomma=removedollar
+    try:
+      if int(wager) == 1:    
+        if int(removecomma) == 100:
+          go = False
+        else:
+          go=True
+      elif int(wager) == 2:
+        if int(removecomma) == 200:
+          go = False
+        else:
+          go=True
+      elif int(wager) == 3:
+        if int(removecomma) == 300:
+          go = False
+        else:
+          go=True
+      elif int(wager) == 4:
+        if int(removecomma) == 400:
+          go = False
+        else:
+          go=True
+      elif int(wager) == 5:
+        if int(removecomma) == 500:
+         go = False
+        else:
+          go=True
+      elif int(wager) == 6:
+       if int(removecomma) == 600:
+          go = False
+       else:
+          go=True      
+      elif int(wager) == 7:
+        if int(removecomma) == 800:
+          go = False
+        else:
+          go=True
+      elif int(wager) == 8:
+        if int(removecomma) == 1000:
+          go = False
+        else:
+          go=True 
+      elif int(wager) == 9:
+        if int(removecomma) == 1200:
+          go = False
+        else:
+          go=True
+      elif int(wager) == 10:
+        if int(removecomma) == 1600:
+          go = False
+        else:
+          go=True
+      elif int(wager) == 11:
+        if int(removecomma) == 2000:
+          go = False
+        else:
+          go=True
+      
+    except:
+      go=True
                  
+quizchoice()
 
 print("Number :"+str(rand)+"\n"
 +"Category: "+quiz['category'][rand]+"\n"
