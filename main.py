afa14ddcd99473a3d78aa3aba0ed712d39bf8722
SubduedCrane651 <95251533+SubdudedCrane651@users.noreply.github.com>
2021-12-06 11:44:23 +0000

    Added messages and functionality

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import random
 import os
 
+#The Jeopardy JSON file at my personal website
+#Where you will find other codes and languages
 url = "https://richard-perreault.com/Documents/JEOPARDY_QUESTIONS1.json"
 
 response = requests.get(url)
@@ -14,6 +16,7 @@ count = len(data)
 quiz = pd.DataFrame(data)
 
 def quizchoice():
+  """Enter in a wager and get a Jeopardy question"""
   os.system('clear')
   print("Enter from 100 to 10000 in increments of 100")
   wager =input("How much do you want to wager ")
@@ -33,16 +36,25 @@ def quizchoice():
        removecomma=0
     
     try:
+      #This will check to see if the number is
+      #an increment of 100
       wager1 = int(wager)/10
       if str(wager1).find('.0') ==-1:
         quizchoice()
         if go:
          return  
+      #Make sure it is between 100 and 10000   
+      if int(wager) < 100.0 or int(wager) > 10000.0:
+        quizchoice()
+        if go:
+         return 
       if int(wager) == int(removecomma):
         go=False
       else:
         continue
     except:
+      #Make sure there is no ValueError so it is just
+      #numbers, and if not ask again
       quizchoice()
       if go:
        return
